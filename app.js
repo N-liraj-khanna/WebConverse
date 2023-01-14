d1d33490d7d668360660d2b5588ead2cc79dfbd1
N Liraj Khanna <lirajkhanna2002@gmail.com>
2023-01-14 20:34:17 +0530

    Connected to Frontend

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+// Import Packages
 const express = require('express');
+
+// Initialise Constants
 const app = express();
 
+// Configurations
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+
+// Routes
 app.get("/",(req, res) => {
-  return res.send("Welcome to Web Converse");
+  return res.render("index");
 })
 
+// Server Setup
 app.listen(process.env.PORT || 3000, ()=>{
   console.log(`Server listening on port ${process.env.PORT||3000}`);
 });
